1a6b31f1bae79fd884c100dc427ab2dc8e1fde6c
Altamash Ansari <ansaaltamash62@gmail.com>
2026-06-07 13:42:05 +0000

    gf

--- scripts/01_Query_Data_SELECT.sql
+++ scripts/01_Query_Data_SELECT.sql
@@ -97,7 +97,7 @@ FROM customers
 ORDER BY country ASC
 
 /* Retrieve all customers and 
-   sort the results by the country and then by the highest score. */
+   sort the results by the country and  then by the highest score. */
 SELECT *
 FROM customers
 ORDER BY country ASC, score DESC
